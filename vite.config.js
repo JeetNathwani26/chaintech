3aba65fc2089de118d04470a5e617ae189f2e9f2
jeetnathwani26 <jeetnathwani660@gmail.com>
2025-10-31 14:03:00 +0530

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
    base: '/chaintech/',
+=======
+  base: '/chaintech/',
+>>>>>>> 1065da1 (Configured gh-pages deployment and updated vite config)
 })
