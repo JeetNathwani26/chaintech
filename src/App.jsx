cb0b2576552c1b1cb4946a18adf10e334634a5b1
jeetnathwani26 <jeetnathwani660@gmail.com>
2025-10-31 21:27:15 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Login from '../component/Login'
 import Register from '../component/Register'
 import Dashboard from '../component/View'
@@ -9,16 +7,16 @@ import Update from '../component/update'
 function App() {
   return (
     <>
-      <BrowserRouter basename="/chaintech">
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/dashboard' element={<Dashboard/>} />
-          <Route path='/update/:id' element={<Update/>}/>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/update/:id' element={<Update />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
 
-export default App;
+export default App
